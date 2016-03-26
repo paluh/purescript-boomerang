670618a3f288a66805a31069023f1205cdee3328
Tomasz Rybarczyk <paluho@gmail.com>
2016-03-26 15:07:39 +0100

    Add catgory for Serializer

--- src/Text/Boomerang/Prim.purs
+++ src/Text/Boomerang/Prim.purs
@@ -4,15 +4,18 @@ import Control.Monad.Eff.Console
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import Text.Parsing.Parser (Parser)
-import Prelude (bind, class Category, class Semigroupoid, id, return, (<<<), (>>=))
+import Prelude (bind, compose, class Category, class Semigroupoid, id, return, (<<<), (>>=))
 
 newtype Serializer tok a b = Serializer (a -> Maybe (Tuple (tok -> tok) b))
 
-data Boomerang tok a b =
-  Boomerang {
-      prs :: Parser tok (a -> Maybe b)
-    , ser :: Serializer tok b a
-  }
+instance semigroupoidSerializer :: Semigroupoid (Serializer tok) where
+  compose (Serializer ser1) (Serializer ser2) = Serializer (\c -> do
+    (Tuple t2 b) <- ser2 c
+    (Tuple t1 a) <- ser1 b
+    return (Tuple (t2 <<< t1) a))
+
+instance categorySerializer :: Category (Serializer tok) where
+  id = Serializer (Just <<< (Tuple id) <<< id)
 
 composePrs :: forall tok a b c. Parser tok (b -> Maybe c) ->
                                 Parser tok (a -> Maybe b) ->
@@ -22,24 +25,22 @@ composePrs prs1 prs2 = do
   b2mc <- prs1
   return (\a -> (a2mb a >>= b2mc))
 
-composeSer :: forall tok a b c. Serializer tok b c ->
-                                Serializer tok a b ->
-                                Serializer tok a c
-composeSer (Serializer ser1) (Serializer ser2) = Serializer (\c -> do
-  (Tuple t2 b) <- ser2 c
-  (Tuple t1 a) <- ser1 b
-  return (Tuple (t2 <<< t1) a))
+data Boomerang tok a b =
+  Boomerang {
+      prs :: Parser tok (a -> Maybe b)
+    , ser :: Serializer tok b a
+  }
 
-instance semigroupoidCategory :: Semigroupoid (Boomerang tok) where
+instance semigroupoidBoomerang :: Semigroupoid (Boomerang tok) where
   compose (Boomerang b1) (Boomerang b2) =
     Boomerang {
         prs : composePrs (b1.prs) (b2.prs)
-      , ser : composeSer (b2.ser) (b1.ser)
+      , ser : compose (b2.ser) (b1.ser)
     }
 
 instance boomerangCategory :: Category (Boomerang tok) where
   id = Boomerang {
       prs : return (Just <<< id)
-    , ser : Serializer (Just <<< (Tuple id) <<< id)
+    , ser : id
   }
 
