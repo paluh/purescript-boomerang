4ace8b220a1f48de0bd38de525cc09a061076114
Tomasz Rybarczyk <paluho@gmail.com>
2016-03-31 21:18:17 +0200

    Add more tests/examples

--- test/Main.purs
+++ test/Main.purs
@@ -2,18 +2,17 @@ module Test.Main where
 
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Error.Util (hush)
 import Data.Generic (class Generic, gEq, gShow)
-import Data.List ((:))
+import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
-import Prelude (bind, class Eq, class Show, compose, return, show, Unit, unit, (<>), (==))
+import Prelude (bind, class Eq, class Show, compose, return, Unit, unit, (<>), (==))
 import Test.Unit (test, runTest, TIMER)
 import Test.Unit.Console (TESTOUTPUT)
-import Test.Unit.Assert (assert)
+import Test.Unit.Assert (assert, equal)
 import Text.Boomerang.HStack (class HList, hArg, hCons, HCons(..), hHead, HNil, hNil, hSingleton, hTop2, HTop2)
-import Text.Boomerang.Combinators (cons, list, nil, opt, pure)
+import Text.Boomerang.Combinators (cons, nil, opt, pure)
 import Text.Boomerang.Prim (Boomerang(..), runSerializer)
 import Text.Boomerang.Routing ((</>))
 import Text.Boomerang.String (int, lit, string, StringBoomerang)
@@ -28,9 +27,9 @@ parse (Boomerang b) s = do
 todo :: forall a. a
 todo = unsafeCoerce unit
 
-serialize :: forall a t. StringBoomerang HNil (HCons a t) -> (HCons a t) -> Maybe String
+serialize :: forall a. StringBoomerang HNil (HCons a HNil) -> a -> Maybe String
 serialize (Boomerang b) s = do
-  (Tuple f _) <- runSerializer b.ser s
+  (Tuple f _) <- runSerializer b.ser (hSingleton s)
   return (f "")
 
 data ProfileViewMode = Compact | Extended
@@ -55,7 +54,8 @@ profileR =
  where
   -- Profile route
   pR :: forall t. StringBoomerang (HTop2 Int ProfileViewMode t) (HCons Profile t)
-  pR = pure (hArg (hArg hCons) (\i v -> Profile { id : i, view : v})) (\(HCons (Profile p) t) -> Just (hTop2 p.id p.view t))
+  pR = pure (hArg (hArg hCons) (\i v -> Profile { id : i, view : v}))
+            (\(HCons (Profile p) t) -> Just (hTop2 p.id p.view t))
 
   -- ProfileView route
   pvR :: forall t. StringBoomerang t (HCons ProfileViewMode t)
@@ -72,32 +72,33 @@ main :: forall e. Eff ( timer :: TIMER
                       , testOutput :: TESTOUTPUT | e
                       ) Unit
 main = runTest do
+  test "String routes parsing" do
+    let foo = (string "foo" :: forall r. StringBoomerang r (HCons String r))
+        bar = (string "bar" :: forall r. StringBoomerang r (HCons String r))
+    equal (Just "foo") (parse foo "foo")
+    equal Nothing (parse foo "bar")
+
+  test "Basic composition with list aggregation" do
+    let fooBar = (cons `compose` string "foo" `compose` cons `compose` string "bar" `compose` nil)
+    test "parsing" do
+      equal (Just ("foo":"bar":Nil)) (parse fooBar "foobar")
+      equal Nothing (parse fooBar "barfoo")
+    test "serialization" do
+      equal (Just "foobar") (serialize fooBar ("foo" : "bar" : Nil))
+      equal Nothing (serialize fooBar ("foo" : "bar" : "baz" : Nil))
+
+  test "Basic alternatives" do
+    let fooOrBar = string "foo" <> string "bar"
+    test "parsing" do
+      equal (Just "bar") (parse fooOrBar "bar")
+      equal (Just "foo") (parse fooOrBar "foo")
+    test "serialization" do
+      equal (Just "foo") (serialize fooOrBar "foo")
+      equal (Just "bar") (serialize fooOrBar "bar")
+
   test "Profile routes" do
     let profile = Profile {id: 20, view: Compact}
-    assert "profile route parsing"
-      (parse profileR "profile/20/compact/" == Just profile)
+    equal (Just profile) (parse profileR "profile/20/compact/")
     assert "profile route parsing without trailing slash"
-      (parse profileR "profile/20/compact" == Just profile)
-    assert "profile route serialization"
-      ((serialize profileR (hSingleton profile)) == Just "profile/20/compact/")
-
-  -- turn these into basic tests
-  -- let foo = (string "foo" :: forall r. StringBoomerang r (HCons String r))
-  --     bar = (string "bar" :: forall r. StringBoomerang r (HCons String r))
-  --     fooBar = (cons `compose` foo `compose` cons `compose` bar `compose` nil)
-  --     fooOrBar = (string "foo" <> string "bar")
-  --     fooOrBarList = list (string "foo" <> string "bar")
-  --     -- fooOrBarList = list fooBar
-
-  -- log "test"
-  -- log (show (parse foo "foo"))
-  -- log (show (parse bar "foo"))
-  -- log (show (parse fooBar "foobar"))
-  -- log (show (parse fooBar "barfoo"))
-  -- log (show (parse fooOrBar "bar"))
-  -- log (show (parse fooOrBar "foo"))
-  -- log (show (serialize fooBar (hSingleton ("foo" : "bar" : Nil))))
-
-  -- log (show (parse fooOrBarList "foofoofoobarfoobar"))
-  -- log (show (serialize fooOrBarList (hSingleton ("bar":"foo":"foo":"bar":Nil))))
-
+      (parse profileR "profile/20/compact/" == Just profile)
+    equal (Just "profile/20/compact/") (serialize profileR profile)
