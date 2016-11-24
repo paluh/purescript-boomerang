9b8db607ca28eb34eba7bd5765aeeea5f299e8bc
Tomasz Rybarczyk <paluho@gmail.com>
2016-11-24 16:21:04 +0100

    Updates for psc-0.10

--- test/Main.purs
+++ test/Main.purs
@@ -4,8 +4,9 @@ import Prelude
 import Test.Text.Boomerang.String as Test.Text.Boomerang.String
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Timer (TIMER)
 import Control.Monad.Eff.Console (CONSOLE)
-import Test.Unit (TIMER, suite)
+import Test.Unit (suite)
 import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
 
